58b11e29b4f3e7852fa893cfd5118a45477f9021
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-28 19:48:02 -0600

    Add js transition on click of panel

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ const panels = document.querySelectorAll('.panel');
 console.log(panels);
 
 function toggleOpen() {
-    console.log('hello');
     this.classList.toggle('open');
 }
 
+function toggleActive(e) {
+   if(e.propertyName.includes('flex')) {
+       this.classList.toggle('open-active');
+   }
+}
+
 panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
+panels.forEach((panel) => panel.addEventListener('transitionend', toggleActive));
